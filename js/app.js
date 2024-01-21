368694457fc1d7336eac36c43d6e3c5a12092fe1
MetroTimur <dildiyana25@gmail.com>
2024-01-22 06:12:43 +0700

    baru3

---
+++ js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("menu", () => ({
+    items: [
+      { id: "1", class: "menu-card geprek", name: "Geprek Sambal Ijo", img: "ijo.jpg", price: 15000 },
+      { id: "2", class: "menu-card geprek", name: "Geprek Sambal Pete", img: "pete.jpg", price: 15000 },
+      { id: "3", class: "menu-card geprek", name: "Geprek Sambal Terasi", img: "terasi.jpg", price: 15000 },
+      { id: "4", class: "menu-card geprek", name: "Geprek Sambal Matah", img: "matah.jpg", price: 15000 },
+      { id: "5", class: "menu-card geprek", name: "Geprek Sambal Jontor", img: "jontor.jpg", price: 15000 },
+    ],
+  }));
+});
--- js/script.js
+++ js/script.js
@@ -13,37 +13,120 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
+// Klik di luar elemen
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
 // filtering Menu Food
 
-const menuBtns = document.querySelectorAll(".menu-btn");
-const foodItems = document.querySelectorAll(".menu-card");
+// const menuBtns = document.querySelectorAll(".menu-btn");
+// const foodItems = document.querySelectorAll(".menu-card");
 
-let activeBtn = "geprek";
+// let activeBtn = "geprek";
 
-showFoodMenu(activeBtn);
+// showFoodMenu(activeBtn);
 
-menuBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    resetActiveBtn();
-    showFoodMenu(btn.id);
-    btn.classList.add("active-btn");
+// menuBtns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     resetActiveBtn();
+//     showFoodMenu(btn.id);
+//     btn.classList.add("active-btn");
+//   });
+// });
+
+// function resetActiveBtn() {
+//   menuBtns.forEach((btn) => {
+//     btn.classList.remove("active-btn");
+//   });
+// }
+
+// function showFoodMenu(newMenuBtn) {
+//   activeBtn = newMenuBtn;
+//   foodItems.forEach((item) => {
+//     if (item.classList.contains(activeBtn)) {
+//       item.style.display = "block";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+// }
+
+// fitur alpine.js
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("menu", () => ({
+    items: [
+      { id: 1, name: "Geprek Sambal Ijo", img: "ijo.jpg", price: 15000 },
+      { id: 2, name: "Geprek Sambal Pete", img: "pete.jpg", price: 15000 },
+      { id: 3, name: "Geprek Sambal Terasi", img: "terasi.jpg", price: 15000 },
+      { id: 4, name: "Geprek Sambal Matah", img: "matah.jpg", price: 15000 },
+      { id: 5, name: "Geprek Sambal Jontor", img: "jontor.jpg", price: 15000 },
+    ],
+  }));
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      const cartItem = this.items.find((item) => item.id === id);
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
   });
 });
 
-function resetActiveBtn() {
-  menuBtns.forEach((btn) => {
-    btn.classList.remove("active-btn");
-  });
-}
-
-function showFoodMenu(newMenuBtn) {
-  activeBtn = newMenuBtn;
-  foodItems.forEach((item) => {
-    if (item.classList.contains(activeBtn)) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
+// konkonverensi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
